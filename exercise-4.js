9d1014b42004391f8388fd8e24adcd1aac35a172
yosaid92 <yohanessahrul92@gmail.com>
2018-01-18 11:30:25 +0700

    Function XO

--- exercise-4.js
+++ exercise-4.js
@@ -1,4 +1,56 @@
-/*     Method 1
+/*
+Diberikan sebuah function xo(str) yang menerima satu parameter berupa string. Function angkan me-return true jika jumlah karakter x sama dengan jumlah karakter o, dan false jika tidak.
+*/
+
+// Pseudocode
+// buat variable 'countX' untuk menampung nilai 'x' dengan nilai awal 0
+// buat variable 'countO' untuk menampung nilai 'o' dengan nilai awal 0
+// looping 'str' dari 0 sampai dengan banyaknya jumlah string
+// jika ketemu karakter 'x' tambahkan 'countX' dengan nilai 1
+// jika ketemu karakter 'o' tambahkan 'countO' dengan nilai 1
+// lalu buat kondisi, jika 'countX' dan 'countO' memiliki nilai yang sama kembalikan nilai 'true'
+// jika sebaliknya kembalikan nilai 'false'
+
+function xo(str) {
+  // you can only write your code here!
+  var countX = 0;
+  var countO = 0;
+  
+  for(var i=0; i<str.length; i++){
+    if(str[i] === 'x'){
+      countX++;
+    } else {
+      countO++;
+    }
+  }
+  
+  if(countX === countO){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// TEST CASES
+console.log(xo('xoxoxo')); // true
+console.log(xo('oxooxo')); // false
+console.log(xo('oxo')); // false
+console.log(xo('xxxooo')); // true
+console.log(xo('xoxooxxo')); // true
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+// with RegExp
 function xo(str){
     var charO = str.match(/o/g);
     var charX = str.match(/x/g);
@@ -12,30 +64,14 @@ function xo(str){
       return false;
     }
   }
-  */
 
-// Method 2
-function xo(str) {
-    var charO = 0;
-    var CharX = 0;
-    
-    for(i=0; i < str.length; i++){
-      if(str[i] === 'o'){
-        charO++;
-      } else if(str[i] === 'x'){
-        CharX++;
-      }
-    }
-    if(CharX === charO){
-      return true;
-    } else {
-      return false;
-    }
-  }
+// TEST CASES
+console.log(xo('xoxoxo')); // true
+console.log(xo('oxooxo')); // false
+console.log(xo('oxo')); // false
+console.log(xo('xxxooo')); // true
+console.log(xo('xoxooxxo')); // true
 
-  
-  // TEST CASES
-  console.log(xo('xoxoxo')); // true
-  console.log(xo('oxo')); // false
-  console.log(xo('xxxooo')); // true
-  console.log(xo('xoxooxxo')); // true
+*/
+
+
