314d4b889d3af47567444bf9130a3a0bd0a98534
yosaid92 <yohanessahrul92@gmail.com>
2018-01-18 11:56:08 +0700

    Palindrome

--- exercise-6.js
+++ exercise-6.js
@@ -1,3 +1,47 @@
+/*
+Diberikan sebuah function palindrome(kata) yang menerima satu parameter. Function tersebut akan mengembalikan nilai true jika kata merupakan palindrome, dan false jika bukan. Kata palindrome adalah sebuah kata yang jika dibalik, tetap sama. Contoh, 'katak' dibalik tetaplah 'katak'.
+*/
+
+// Pseudocode
+// ( balik dulu - lalu kondisikan )
+// buat variable 'tampung' dengan tipe data string
+// looping 'kata' dari kanan ke kiri sebanyak jumlah karakter pada 'kata'
+// tampung tiap karakternya pada variable 'tampung'
+// kodisikan === 
+// jika 'tampung' sama dengan 'kata' maka kembalikan nilai true
+// jika sebaliknya kembalikan nilai false
+
+function palindrome(kata) {
+  // you can only write your code here!
+  var tampung = '';
+  for(var i=kata.length-1; i >= 0; i--){
+    tampung += kata[i];
+  }
+  
+  if(tampung === kata){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// TEST CASES
+console.log(palindrome('katak')); // true
+console.log(palindrome('blanket')); // false
+console.log(palindrome('civic')); // true
+console.log(palindrome('kasur rusak')); // true
+console.log(palindrome('mister')); // false
+
+
+
+
+
+
+
+
+
+
+/*
 function palindrome(kata) {
     var arr = kata.split('');
     var balik = arr.reverse();
@@ -22,3 +66,5 @@ console.log("======================");
 console.log(palindrome('kasur rusak')); // true
 console.log("======================");
 console.log(palindrome('mister')); // false
+
+*/
