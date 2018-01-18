68cc9f6af2cf58e6cb3888e376887f8cc40ac053
yosaid92 <yohanessahrul92@gmail.com>
2018-01-18 11:18:40 +0700

    Konversi Menit

--- exercise-3.js
+++ exercise-3.js
@@ -1,4 +1,30 @@
+/*
+Diberikan sebuah function konversiMenit(menit) yang menerima satu parameter berupa angka yang merupakan ukuran waktu dalam menit. Function akan me-return string waktu dalam format jam:menit berdasarkan menit tersebut. Contoh, jika menit adalah 63, maka function akan me-return "1:03".
+*/
+
 function konversiMenit(menit) {
+  // you can only write your code here!
+  var jams = Math.floor(menit / 60);
+  var menits = menit % 60;
+  
+  if(menits < 10){
+    return jams + ':0' + menits; 
+  } else {
+    return jams + ':' + menits; 
+  }
+}
+
+// TEST CASES
+console.log(konversiMenit(63)); // 1:03
+console.log(konversiMenit(124)); // 2:04
+console.log(konversiMenit(53)); // 0:53
+console.log(konversiMenit(88)); // 1:28
+console.log(konversiMenit(120)); // 2:00
+
+
+
+
+/*function konversiMenit(menit) {
     var minute = Math.floor(menit / 60);
     var second = menit - (minute * 60);
     second = ('0' + second).slice(-2);
@@ -12,3 +38,4 @@ console.log(konversiMenit(124)); // 2:04
 console.log(konversiMenit(53)); // 0:53
 console.log(konversiMenit(88)); // 1:28
 console.log(konversiMenit(120)); // 2:00  
+*/
